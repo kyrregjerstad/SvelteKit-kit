17c4bf1b2239068a3e73536eaf3d6a9cee004310
Kyrre Gjerstad <kyrregjerstad@gmail.com>
2025-08-04 21:30:27 +0200

    updates

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit-kit!</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+  let { data } = $props();
+</script>
+
+<div class="flex flex-col items-center justify-center h-screen">
+	<h1 class="text-4xl font-bold">Welcome to SvelteKit-kit!</h1>
+	<p class="text-lg">Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+</div>
+
